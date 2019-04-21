2c95c07956b0637adb68ce9ec2ffbcaa72cab347
Paul Tiller <pftiller@gmail.com>
2019-04-21 12:14:49 -0500

    new build

---
+++ build/precache-manifest.d3552c57442ead93c273c603746dc023.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3c3b9b44ade378ccf359",
+    "url": "/static/js/main.18cd357e.chunk.js"
+  },
+  {
+    "revision": "ff6bae633bd813807c99",
+    "url": "/static/js/2.8e45ebbe.chunk.js"
+  },
+  {
+    "revision": "3c3b9b44ade378ccf359",
+    "url": "/static/css/main.7543868b.chunk.css"
+  },
+  {
+    "revision": "a1e0e28a399d68d237bc798baa11384f",
+    "url": "/index.html"
+  }
+];
