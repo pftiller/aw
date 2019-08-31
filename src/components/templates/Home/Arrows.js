d95b1755a791c9eab3374aa9f32ea820293dddf6
Paul Tiller <pftiller@gmail.com>
2019-08-31 13:45:32 -0500

    having timer reset upon arrow clicks

--- src/components/templates/Home/Arrows.js
+++ src/components/templates/Home/Arrows.js
@@ -1,68 +1,91 @@
 import React from 'react';
-import { InnerImage } from './InnerImage';
+import {images} from '../../../js/carousel-images';
 
 export class Arrows extends React.Component {
-    
-    constructor(props){
-        super(props);
-        this.state = {index: 0}; 
-        this.handleClick = this.handleClick.bind(this);
-    }
-    componentDidMount() {
-        this.interval = setInterval(()=> {
+
+        constructor(props) {
+            super(props);
+            this.state = {
+                index: this.props.index
+            };
+            // this.handleClick = this.handleClick.bind(this);
+            this.forward = this.forward.bind(this);
+            this.back = this.back.bind(this);
+        }
+        static getDerivedStateFromProps(props, state) {
+            if (props.time !== state.time) {
+                return {
+                    index: props.index + 1
+                }
+            }
+            return null;
+        }
+        componentDidMount() {
+            this.timer = null;
+            this.timer = setInterval(() => {
+                this.forward();
+              }, 8000);
+          }
+         
+        handleClick(e) {
+            let className = e.target.className;
+            if (className === 'left-arrow') {
+                this.back();
+            } else {
+                this.forward();
+            }
+        }
+        back() {
+            clearInterval(this.timer);
             let index = this.state.index;
-            if(index < 2) {
-                index +=1
-                this.setState({index: index});
+            if (index > 0) {
+                index -= 1
+                this.setState({
+                    index: index
+                });
+            } else {
+                this.setState({
+                    index: 2
+                });
             }
-            else {
-                this.setState({index: 0});
+            this.timer = setInterval(() => {
+                this.forward();
+              }, 8000);
+        }
+        forward() {
+            clearInterval(this.timer);
+            let index = this.state.index;
+            if (index < 2) {
+                index += 1
+                this.setState({
+                    index: index
+                });
+            } else {
+                this.setState({
+                    index: 0
+                });
             }
-        },
-        8000);
-    }
-    handleClick(e){
-        let className = e.target.className;
-        if(className === 'left-arrow') {
-            this.back();
-            console.log(className);
+            this.timer = setInterval(() => {
+                this.forward();
+              }, 8000);
         }
-        else {
-            console.log(className);    
-            this.forward();
+        componentWillUnmount() {
+            clearInterval(this.timer);
         }
-    }
-    back(){
-        let index = this.state.index;
-        if(index > 0) {
-            index -=1
-            this.setState({index: index});
-        }
-        else {
-            this.setState({index: 2});
-        }
-    }
-    forward(){
-        let index = this.state.index;
-        if(index < 2) {
-            index +=1
-            this.setState({index: index});
-        }
-        else {
-            this.setState({index: 0});
-        }
-    }
-    componentWillUnmount() {
-        clearInterval(this.interval);
-      }
     render() {
         return(
             <div className="buttons">
-                <img src='./images/svgs/left-chevron.svg' alt="Left Chevron" className="left-arrow" onClick={this.handleClick}/>
-                <img src='./images/svgs/right-chevron.svg' alt="Right Chevron" className="right-arrow" onClick={this.handleClick}/>
-                <InnerImage index={this.state.index}/>
+            <img src='./images/svgs/left-chevron.svg' alt="Left Chevron" className="left-arrow" onClick={(e) => this.handleClick(e)}/> 
+            <img src='./images/svgs/right-chevron.svg' alt="Right Chevron" className="right-arrow" onClick={(e) => this.handleClick(e)}/> 
+            <div className="slide" style={images[this.state.index].styles}>
+                <div className="grey-backdrop">
+                    <div className="caption">
+                    <h3>{images[this.state.index].header}</h3>
+                    <p>{images[this.state.index].caption}</p>
+                    </div>
+                </div>
+            </div>
             </div>
         )
     }
 }
-
--- src/components/templates/Home/Carousel.js
+++ src/components/templates/Home/Carousel.js
@@ -2,8 +2,15 @@ import React from 'react';
 import {Arrows} from './Arrows';
 let green = '/images/green.png';
 
-
 export class Carousel extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        index: 0
+      };
+    }
+
+
     render() {
      let styles = {
         background: `url(${green}) bottom / 107% 94% no-repeat`
@@ -11,7 +18,7 @@ export class Carousel extends React.Component {
       return (
         <div id="home" className="carousel" style={styles}>
             <h2 className="green">Welcome to ArtWrite Productions</h2>
-            <Arrows/>
+            <Arrows index={this.state.index}/>
         </div>
       );
     };
