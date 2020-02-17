dade9beb5afc5b89c9334361b66438903155904d
Paul Tiller <pftiller@gmail.com>
2020-02-17 11:01:25 -0600

    webp logo

--- src/components/templates/Home/Header.js
+++ src/components/templates/Home/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Picture } from 'react-responsive-picture';
 import {Nav} from './Nav';
 const hamburgerStates = require('../../../js/hamburger-states');
 export class HeaderContents extends React.Component {
@@ -52,7 +53,20 @@ export class HeaderContents extends React.Component {
         if(this.state.width < 1200 && this.state.condition === "open") {
             return(
                 <div>
-                    <img src='./images/artwrite-logo.png' alt="ArtWrite Productions Logo" className="logo"/>
+                    < Picture
+                        className = "logo"
+                        alt = "ArtWrite Productions Logo"
+                        sources = {
+                            [{
+                                    srcSet: "/images/artwrite-logo.png",
+                                },
+                                {
+                                    srcSet: "/images/artwrite-logo.webp",
+                                    type: "image/webp"
+                                }
+                            ]
+                        }
+                    />
                     <img src={hamburgerStates.open.src} className={hamburgerStates.open.className} alt={hamburgerStates.open.alt} onClick={() => this.toggleHamburger()}/>
                     <Nav condition={this.state.condition}/>
                 </div>
@@ -61,7 +75,20 @@ export class HeaderContents extends React.Component {
         else if (this.state.width < 1200 && this.state.condition === "closed") {
             return(
                 <div>
-                    <img src='./images/artwrite-logo.png' alt="ArtWrite Productions Logo" className="logo"/>
+                    < Picture
+                        className = "logo"
+                        alt = "ArtWrite Productions Logo"
+                        sources = {
+                            [{
+                                    srcSet: "/images/artwrite-logo.png",
+                                },
+                                {
+                                    srcSet: "/images/artwrite-logo.webp",
+                                    type: "image/webp"
+                                }
+                            ]
+                        }
+                    />
                     <img src={hamburgerStates.closed.src} className={hamburgerStates.closed.className} alt={hamburgerStates.closed.alt} onClick={() => this.toggleHamburger()}/>
                     <Nav condition={this.state.condition}/>
                 </div>
@@ -70,7 +97,20 @@ export class HeaderContents extends React.Component {
         else {
             return(
                 <div>
-                    <img src='./images/artwrite-logo.png' alt="ArtWrite Productions Logo" className="logo"/>
+                    < Picture
+                        className = "logo"
+                        alt = "ArtWrite Productions Logo"
+                        sources = {
+                            [{
+                                    srcSet: "/images/artwrite-logo.png",
+                                },
+                                {
+                                    srcSet: "/images/artwrite-logo.webp",
+                                    type: "image/webp"
+                                }
+                            ]
+                        }
+                    />
                     <Nav condition={this.state.condition}/>
                 </div>
             )
