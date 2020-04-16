917604d12041061b3ee0bf21073c01197cc1d965
Paul Tiller <pftiller@gmail.com>
2020-04-16 09:35:07 -0500

    refactor

---
+++ server.js
@@ -0,0 +1,15 @@
+// require('@google-cloud/debug-agent').start();
+const express = require("express");
+// const path = require('path');
+const app = express();
+
+app.use(express.static('build'));
+
+const PORT = process.env.PORT || 3000;
+
+
+// Loadable.preloadAll().then(() => {
+	app.listen(PORT, (error) => {
+		console.log("listening on " + PORT + "...");
+	});
+// });
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const {GenerateSW} = require('workbox-webpack-plugin');
+
+module.exports = {
+    context: __dirname,
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'js/bundle.js',
+        publicPath: '/',
+    },
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.join(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|j?g|jpe?g|svg|gif)$/i,
+                include: path.join(__dirname, 'images'),
+                use: 'file-loader',
+                use: 'webp-loader'
+            },
+            {
+                test: /\.json$/,
+                loader: 'json-loader',
+                type: 'javascript/auto',
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/react', ],
+                    plugins: ['@babel/plugin-proposal-class-properties']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "index.html",
+            hash: true
+        }),
+        new GenerateSW({
+            swDest: 'sw.js'
+        })
+    ],
+}
